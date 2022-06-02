d641d25945dd3cb370b8ea245a2e6dc57bd67697
vladimirzhinev <vladimirzhinev@gmail.com>
2022-06-02 06:16:34 +0300

    fix: fix tabs in html according to rule 1.1 in html-and-css.md

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ var createNewTaskElement = function (taskString) {
 
   deleteButton.className = 'delete button';
   deleteButtonImg.src = './remove.svg';
+  deleteButtonImg.className = 'img';
+
   deleteButtonImg.alt = 'remove icon';
 
   deleteButton.appendChild(deleteButtonImg);
