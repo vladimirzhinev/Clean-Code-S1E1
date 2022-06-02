e33b48b9d23d2cc13ded5cbb0cfd3a10547947ad
vladimirzhinev <vladimirzhinev@gmail.com>
2022-06-02 05:54:06 +0300

    fix: fix several class names in html,css and js according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput = document.getElementById('new-task');//Add a new task.
-var addButton = document.querySelector('.addBtn');//first button
+var addButton = document.querySelector('.add-btn');//first button
 var incompleteTaskHolder = document.getElementById('incomplete-tasks');//ul of #incompleteTasks
 var completedTasksHolder = document.getElementById('completed-tasks');//completed-tasks
 
